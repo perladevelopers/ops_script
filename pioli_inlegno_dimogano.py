5a1da27781db34b85bbbc9b74e761423c72e7d17
perladevelopers <121440938+perladevelopers@users.noreply.github.com>
2023-01-26 20:09:27 +0100

    Update pioli_inlegno_dimogano.py

--- pioli_inlegno_dimogano.py
+++ pioli_inlegno_dimogano.py
@@ -522,209 +522,209 @@ def ascesnore():
 
 
 def canali():
-   canali = str(input('canali: '))
-   direzioni = str(input('direzioni: '))
-   lista_canali = canali.split(' ')
-   lista_direzioni = direzioni.split(' ')
-   #lista_direzioni = ['ac', 'bc', 'fe', 'cd', 'ge']
-   #lista_canali = ['0', '0', '11.8', '4', '5', '2', '7.3']
-   d = 0
+       canali = str(input('canali: '))
+       direzioni = str(input('direzioni: '))
+       lista_canali = canali.split(' ')
+       lista_direzioni = direzioni.split(' ')
+       #lista_direzioni = ['ac', 'bc', 'fe', 'cd', 'ge']
+       #lista_canali = ['0', '0', '11.8', '4', '5', '2', '7.3']
+       d = 0
 
-   a = 0
-   b = 0
-   c = 0
-   d = 0
-   e = 0
-   f = 0
-   g = 0
-   h = 0
-   ii = 0
-   m = 0
+       a = 0
+       b = 0
+       c = 0
+       d = 0
+       e = 0
+       f = 0
+       g = 0
+       h = 0
+       ii = 0
+       m = 0
 
-   alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'l', 'm']
+       alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'l', 'm']
 
-   def count(v):
-       C = 0
-       for j in lista_direzioni:
-           if j[0] == v:
-               C += 1
+       def count(v):
+           C = 0
+           for j in lista_direzioni:
+               if j[0] == v:
+                   C += 1
 
-       return C
+           return C
 
-   for i, valore in enumerate(lista_canali):
-       if i == 0:
-           a = float(valore)
-       elif i == 1:
-           b = float(valore)
-       elif i == 2:
-           c = float(valore)
-       elif i == 3:
-           d = float(valore)
-       elif i == 4:
-           e = float(valore)
-       elif i == 5:
-           f = float(valore)
-       elif i == 6:
-           g = float(valore)
-       elif i == 7:
-           h = float(valore)
-       elif i == 8:
-           ii = float(valore)
-       elif i == 9:
-           m = float(valore)
+       for i, valore in enumerate(lista_canali):
+           if i == 0:
+               a = float(valore)
+           elif i == 1:
+               b = float(valore)
+           elif i == 2:
+               c = float(valore)
+           elif i == 3:
+               d = float(valore)
+           elif i == 4:
+               e = float(valore)
+           elif i == 5:
+               f = float(valore)
+           elif i == 6:
+               g = float(valore)
+           elif i == 7:
+               h = float(valore)
+           elif i == 8:
+               ii = float(valore)
+           elif i == 9:
+               m = float(valore)
 
-   for i in lista_direzioni:
-       if i[1] == 'a':
-           if i[0] == 'b': a += (a / count('b'))
-           elif i[0] == 'c': a += (c / count('c'))
-           elif i[0] == 'd': a += (d / count('d'))
-           elif i[0] == 'e': a += (e / count('e'))
-           elif i[0] == 'f': a += (f / count('f'))
-           elif i[0] == 'g': a += (g / count('g'))
-           elif i[0] == 'h': a += (h / count('h'))
-           elif i[0] == 'i': a += (ii / count('i'))
-           elif i[0] == 'm': a += (m / count('m'))
-       elif i[1] == 'b':
-           if i[0] == 'a': b += (a / count('a'))
-           elif i[0] == 'c': b += (c / count('c'))
-           elif i[0] == 'd': b += (d / count('d'))
-           elif i[0] == 'e': b += (e / count('e'))
-           elif i[0] == 'f': b += (f / count('f'))
-           elif i[0] == 'g': b += (g / count('g'))
-           elif i[0] == 'h': b += (h / count('h'))
-           elif i[0] == 'i': b += (ii / count('i'))
-           elif i[0] == 'm': b += (m / count('m'))
-       elif i[1] == 'c':
-           if i[0] == 'a':
-               c += (a / count('a'))
-               print(i[0], a / count('a'))
-           elif i[0] == 'b':
-               c += (b / count('b'))
-               print(i[0], b / count('b'))
-           elif i[0] == 'd': c += (d / count('d'))
-           elif i[0] == 'e': c += (e / count('e'))
-           elif i[0] == 'f': c += (f / count('f'))
-           elif i[0] == 'g': c += (g / count('g'))
-           elif i[0] == 'h': c += (h / count('h'))
-           elif i[0] == 'i': c += (ii / count('i'))
-           elif i[0] == 'm': c += (m / count('m'))
-       elif i[1] == 'd':
-           if i[0] == 'a': d += (a / count('a'))
-           elif i[0] == 'b': d += (b / count('b'))
-           elif i[0] == 'c':
-               d += (c / count('c'))
-               print(i[0], c / count('c'))
-           elif i[0] == 'e': d += (e / count('e'))
-           elif i[0] == 'f':
-               d += (f / count('f'))
-               print(i[0], f / count('f'))
-           elif i[0] == 'g': d += (g / count('g'))
-           elif i[0] == 'h': d += (h / count('h'))
-           elif i[0] == 'i': d += (ii / count('i'))
-           elif i[0] == 'm': d += (m / count('m'))
-       elif i[1] == 'e':
-           if i[0] == 'a': e += (a / count('a'))
-           elif i[0] == 'b': e += (b / count('b'))
-           elif i[0] == 'c': e += (c / count('c'))
-           elif i[0] == 'd': e += (d / count('d'))
-           elif i[0] == 'f': e += (f / count('f'))
-           elif i[0] == 'g':
-               e += (g / count('g'))
-               print(i[0], g / count('g'))
-           elif i[0] == 'h': e += (h / count('h'))
-           elif i[0] == 'i': e += (ii / count('i'))
-           elif i[0] == 'm': e += (m / count('m'))
-       elif i[1] == 'f':
-           if i[0] == 'a': f += (a / count('a'))
-           elif i[0] == 'b': f += (b / count('b'))
-           elif i[0] == 'c': f += (c / count('c'))
-           elif i[0] == 'd': f += (d / count('d'))
-           elif i[0] == 'e': f += (e / count('e'))
-           elif i[0] == 'g': f += (g / count('g'))
-           elif i[0] == 'h': f += (h / count('h'))
-           elif i[0] == 'i': f += (ii / count('i'))
-           elif i[0] == 'm': f += (m / count('m'))
-       elif i[1] == 'g':
-           if i[0] == 'a': g += (a / count('a'))
-           elif i[0] == 'b': g += (b / count('b'))
-           elif i[0] == 'c': g += (c / count('c'))
-           elif i[0] == 'd': g += (d / count('d'))
-           elif i[0] == 'e': g += (e / count('e'))
-           elif i[0] == 'f': g += (f / count('f'))
-           elif i[0] == 'h': g += (h / count('h'))
-           elif i[0] == 'i': g += (ii / count('i'))
-           elif i[0] == 'm': g += (m / count('m'))
-       elif i[1] == 'h':
-           if i[0] == 'a': h += (a / count('a'))
-           elif i[0] == 'b': h += (b / count('b'))
-           elif i[0] == 'c': h += (c / count('c'))
-           elif i[0] == 'd': h += (d / count('d'))
-           elif i[0] == 'e': h += (e / count('e'))
-           elif i[0] == 'f': h += (f / count('f'))
-           elif i[0] == 'g': h += (g / count('g'))
-           elif i[0] == 'i': h += (ii / count('i'))
-           elif i[0] == 'm': h += (m / count('m'))
-       elif i[1] == 'i':
-           if i[0] == 'a': ii += (a / count('a'))
-           elif i[0] == 'b': ii += (b / count('b'))
-           elif i[0] == 'c': ii += (c / count('c'))
-           elif i[0] == 'd': ii += (d / count('d'))
-           elif i[0] == 'e': ii += (e / count('e'))
-           elif i[0] == 'f': ii += (f / count('f'))
-           elif i[0] == 'g': ii += (g / count('g'))
-           elif i[0] == 'h': ii += (h / count('h'))
-           elif i[0] == 'm': ii += (m / count('m'))
-       elif i[1] == 'm':
-           if i[0] == 'a': m += (a / count('a'))
-           elif i[0] == 'b': m += (b / count('b'))
-           elif i[0] == 'c': m += (c / count('c'))
-           elif i[0] == 'd': m += (d / count('d'))
-           elif i[0] == 'e': m += (e / count('e'))
-           elif i[0] == 'f': m += (f / count('f'))
-           elif i[0] == 'g': m += (g / count('g'))
-           elif i[0] == 'h': m += (h / count('h'))
-           elif i[0] == 'i': m += (ii / count('i'))
-       print(i)
+       for i in lista_direzioni:
+           if i[1] == 'a':
+               if i[0] == 'b': a += (a / count('b'))
+               elif i[0] == 'c': a += (c / count('c'))
+               elif i[0] == 'd': a += (d / count('d'))
+               elif i[0] == 'e': a += (e / count('e'))
+               elif i[0] == 'f': a += (f / count('f'))
+               elif i[0] == 'g': a += (g / count('g'))
+               elif i[0] == 'h': a += (h / count('h'))
+               elif i[0] == 'i': a += (ii / count('i'))
+               elif i[0] == 'm': a += (m / count('m'))
+           elif i[1] == 'b':
+               if i[0] == 'a': b += (a / count('a'))
+               elif i[0] == 'c': b += (c / count('c'))
+               elif i[0] == 'd': b += (d / count('d'))
+               elif i[0] == 'e': b += (e / count('e'))
+               elif i[0] == 'f': b += (f / count('f'))
+               elif i[0] == 'g': b += (g / count('g'))
+               elif i[0] == 'h': b += (h / count('h'))
+               elif i[0] == 'i': b += (ii / count('i'))
+               elif i[0] == 'm': b += (m / count('m'))
+           elif i[1] == 'c':
+               if i[0] == 'a':
+                   c += (a / count('a'))
+                   print(i[0], a / count('a'))
+               elif i[0] == 'b':
+                   c += (b / count('b'))
+                   print(i[0], b / count('b'))
+               elif i[0] == 'd': c += (d / count('d'))
+               elif i[0] == 'e': c += (e / count('e'))
+               elif i[0] == 'f': c += (f / count('f'))
+               elif i[0] == 'g': c += (g / count('g'))
+               elif i[0] == 'h': c += (h / count('h'))
+               elif i[0] == 'i': c += (ii / count('i'))
+               elif i[0] == 'm': c += (m / count('m'))
+           elif i[1] == 'd':
+               if i[0] == 'a': d += (a / count('a'))
+               elif i[0] == 'b': d += (b / count('b'))
+               elif i[0] == 'c':
+                   d += (c / count('c'))
+                   print(i[0], c / count('c'))
+               elif i[0] == 'e': d += (e / count('e'))
+               elif i[0] == 'f':
+                   d += (f / count('f'))
+                   print(i[0], f / count('f'))
+               elif i[0] == 'g': d += (g / count('g'))
+               elif i[0] == 'h': d += (h / count('h'))
+               elif i[0] == 'i': d += (ii / count('i'))
+               elif i[0] == 'm': d += (m / count('m'))
+           elif i[1] == 'e':
+               if i[0] == 'a': e += (a / count('a'))
+               elif i[0] == 'b': e += (b / count('b'))
+               elif i[0] == 'c': e += (c / count('c'))
+               elif i[0] == 'd': e += (d / count('d'))
+               elif i[0] == 'f': e += (f / count('f'))
+               elif i[0] == 'g':
+                   e += (g / count('g'))
+                   print(i[0], g / count('g'))
+               elif i[0] == 'h': e += (h / count('h'))
+               elif i[0] == 'i': e += (ii / count('i'))
+               elif i[0] == 'm': e += (m / count('m'))
+           elif i[1] == 'f':
+               if i[0] == 'a': f += (a / count('a'))
+               elif i[0] == 'b': f += (b / count('b'))
+               elif i[0] == 'c': f += (c / count('c'))
+               elif i[0] == 'd': f += (d / count('d'))
+               elif i[0] == 'e': f += (e / count('e'))
+               elif i[0] == 'g': f += (g / count('g'))
+               elif i[0] == 'h': f += (h / count('h'))
+               elif i[0] == 'i': f += (ii / count('i'))
+               elif i[0] == 'm': f += (m / count('m'))
+           elif i[1] == 'g':
+               if i[0] == 'a': g += (a / count('a'))
+               elif i[0] == 'b': g += (b / count('b'))
+               elif i[0] == 'c': g += (c / count('c'))
+               elif i[0] == 'd': g += (d / count('d'))
+               elif i[0] == 'e': g += (e / count('e'))
+               elif i[0] == 'f': g += (f / count('f'))
+               elif i[0] == 'h': g += (h / count('h'))
+               elif i[0] == 'i': g += (ii / count('i'))
+               elif i[0] == 'm': g += (m / count('m'))
+           elif i[1] == 'h':
+               if i[0] == 'a': h += (a / count('a'))
+               elif i[0] == 'b': h += (b / count('b'))
+               elif i[0] == 'c': h += (c / count('c'))
+               elif i[0] == 'd': h += (d / count('d'))
+               elif i[0] == 'e': h += (e / count('e'))
+               elif i[0] == 'f': h += (f / count('f'))
+               elif i[0] == 'g': h += (g / count('g'))
+               elif i[0] == 'i': h += (ii / count('i'))
+               elif i[0] == 'm': h += (m / count('m'))
+           elif i[1] == 'i':
+               if i[0] == 'a': ii += (a / count('a'))
+               elif i[0] == 'b': ii += (b / count('b'))
+               elif i[0] == 'c': ii += (c / count('c'))
+               elif i[0] == 'd': ii += (d / count('d'))
+               elif i[0] == 'e': ii += (e / count('e'))
+               elif i[0] == 'f': ii += (f / count('f'))
+               elif i[0] == 'g': ii += (g / count('g'))
+               elif i[0] == 'h': ii += (h / count('h'))
+               elif i[0] == 'm': ii += (m / count('m'))
+           elif i[1] == 'm':
+               if i[0] == 'a': m += (a / count('a'))
+               elif i[0] == 'b': m += (b / count('b'))
+               elif i[0] == 'c': m += (c / count('c'))
+               elif i[0] == 'd': m += (d / count('d'))
+               elif i[0] == 'e': m += (e / count('e'))
+               elif i[0] == 'f': m += (f / count('f'))
+               elif i[0] == 'g': m += (g / count('g'))
+               elif i[0] == 'h': m += (h / count('h'))
+               elif i[0] == 'i': m += (ii / count('i'))
+           print(i)
 
-   print(f' a:{a}, b:{b}, c:{c}, d:{d}, e:{e}, f:{f}, g:{g}, h:{h}, i:{ii}, l:{m}')
-   totale = (a,b,c,d,e,f,g,h,ii,m)
-   print(max(totale))
+       print(f' a:{a}, b:{b}, c:{c}, d:{d}, e:{e}, f:{f}, g:{g}, h:{h}, i:{ii}, l:{m}')
+       totale = (a,b,c,d,e,f,g,h,ii,m)
+       print(max(totale))
 
-   r = 'ab bc bd ce cf df dg di eh fh lh hm'
-   h = '8 4 2 3 6 9 6 1 1 10'
+       r = 'ab bc bd ce cf df dg di eh fh lh hm'
+       h = '8 4 2 3 6 9 6 1 1 10'
 
-# corriere()
-# attivita() #max 6
-# crittografia()
-# canali()
+    # corriere()
+    # attivita() #max 6
+    # crittografia()
+    # canali()
 
-st.header('OPS cracker')
-want = st.selectbox('What do u wanna do?', ('Attività', 'Corriere', 'Crittografia', 'Linguaggio'))
+    st.header('OPS cracker')
+    want = st.selectbox('What do u wanna do?', ('Attività', 'Corriere', 'Crittografia', 'Linguaggio'))
 
-print = st.write
-input = st.text_input
-if want == 'Attività':
-   try:
-       attivita()
-   except:
-       pass
-elif want == 'Corriere':
-   try:
-       corriere()
-   except:
-       pass
-elif want == 'Crittografia':
-   try:
-       crittografia()
-   except:
-       pass
-elif want == 'Linguaggio':
-   interpreter()
-  
-elif want == 'Canali':
-   interpreter()
-   try:
-       canali()
-   except:
-       pass
+    print = st.write
+    input = st.text_input
+    if want == 'Attività':
+       try:
+           attivita()
+       except:
+           pass
+    elif want == 'Corriere':
+       try:
+           corriere()
+       except:
+           pass
+    elif want == 'Crittografia':
+       try:
+           crittografia()
+       except:
+           pass
+    elif want == 'Linguaggio':
+       interpreter()
+
+    elif want == 'Canali':
+       interpreter()
+       try:
+           canali()
+       except:
+           pass
 
